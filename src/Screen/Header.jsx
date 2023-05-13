ee4ab3ee87fa4b4a84cfc8c8a079a3e3cd798e82
julesgaborit <jules.gaborit@my-digital-school.org>
2023-05-13 17:08:55 +0200

    nouveau composant

--- src/Screen/Header.jsx
+++ src/Screen/Header.jsx
@@ -6,14 +6,14 @@ import panier from '../assets/icon/panier.svg';
 export const Header = () => {
     return (
         <header className='flex wrap mb-40 pt-10'>
-                <img src={logo} className="ml-20 w-2/12" alt="logo cyber champions" />
+                <a href="/"><img src={logo} className="ml-20 w-11/12" alt="logo cyber champions" /></a>
             <nav className=' flex ml-40'>
                 <a className="text-white mt-5" href="/">Accueil</a>
                 <a className="text-white mt-5 ml-10" href="/notrejeu">Notre jeu</a>
                 <a className="text-white mt-5 ml-10" href="/apropos">A propos</a>
                 <a className="text-white mt-5 ml-10" href="/contact">Nous contacter</a>
                 <div className='flex ml-40'>
-                <a href="/"><img className="w-6/12 ml-10" src={compte} alt="" /></a>
+                <a href="/compte"><img className="w-6/12 ml-10" src={compte} alt="" /></a>
                 <a href="/"><img className="w-6/12 ml-10"src={panier} alt="" /></a>
                 </div>
             </nav>
